70b6972f680071d69e6b19c883935e4dbafcd14b
ocawthorne <ocawthorne@gmail.com>
2021-01-21 17:13:46 +0000

    basic-functions-review

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function mondayWork(action="go to the office") {
+   return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(str="*") {
+   return function(param="special") {
+      return `You are ${str+param+str}!`
+   }
+}
+
+function saturdayFun(arg="roller-skate") {
+   return `This Saturday, I want to ${arg}!`   
+}
+
+let Calculator = {
+   add: function() { return 1 + 3 },
+   subtract: function() { return 1 - 3 },
+   multiply: function() { return 1 * 3 },
+   divide: function() { return 10 / 5 }
+}
+
+function actionApplyer(int=0, arr=[]) {
+   if (arr.length === 0) return int
+   return arr[2](arr[1](arr[0](int)))
+}
